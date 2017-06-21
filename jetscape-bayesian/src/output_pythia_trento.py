352ac070525c1ff61ec7675b84a5e1a15fd511cb
Matthias Heinz <heinz.matthias.3@gmail.com>
2017-06-21 11:03:01 -0700

    Change behavior of file arguments and generate TRENTO data in bulk

--- jetscape-bayesian/src/output_pythia_trento.py
+++ jetscape-bayesian/src/output_pythia_trento.py
@@ -3,12 +3,27 @@ from __future__ import division
 from __future__ import print_function
 import sys
 import os
+import errno
 import math
 import random
 import pythia8
 import numpy as np
 import argparse
 import subprocess
+from time import gmtime, strftime
+
+
+def _create_dir(path):
+    if path[0] == '~':
+        path = path[2:]
+        home = os.path.expanduser('~')
+        path = os.path.join(home, path)
+    try:
+        os.makedirs(path)
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise
+    return str(os.path.abspath(path))
 
 
 # -------------------------- ARGUMENT PARSING ------------------------ #
@@ -27,7 +42,7 @@ parser = argparse.ArgumentParser(
 
 # Add arguments
 parser.add_argument('-f', '--file', type=str, metavar='TRENTO_FILE',
-                    default='../data/AuAu_200GeV_100k.txt',
+                    default='off',
                     help='path to TRENTO data file or turn \"off\"')
 parser.add_argument('-e', '--eCM', type=float, default=200.0,
                     metavar='BEAM_ENERGY',
@@ -40,7 +55,7 @@ parser.add_argument('-c', '--QCDoff', action='store_true',
 parser.add_argument('-q', '--QEDoff', action='store_true',
                     help='turn PYTHIA hard QED processes off')
 parser.add_argument('-o', '--output', type=str, metavar='OUTPUT_FILE',
-                    default='output.txt',
+                    default='auto',
                     help='path to output file')
 parser.add_argument('-s', '--seed', type=int, default=-1,
                     help='seed for random numbers in PYTHIA')
@@ -58,7 +73,7 @@ parser.add_argument('-z', '--pTHatMax', type=float, default=25.0,
 parser.add_argument('-w', '--filter', action='store_true',
                     help='Enable filtering results')
 parser.add_argument('-x', '--filter-file', type=str,
-                    metavar='FILTER_FILE', default='filter.txt',
+                    metavar='FILTER_FILE', default='auto',
                     help='Set path to file for filtered results')
 
 # Parse arguments
@@ -93,25 +108,45 @@ if args.pTHatMax <= args.pTHatMin:
 if args.output == args.file:
     raise ValueError('Cannot read from and output to same file')
 
-if args.filter and args.filter_file == args.output:
+if args.filter and args.filter_file == args.output and args.output != 'auto':
     raise ValueError('Filter file cannot be the same as output file')
 
 if args.filter and args.filter_file == args.file:
     raise ValueError('Cannot read from and filter to the same file')
 
-if not args.filter and args.filter_file != 'filter.txt':
+if not args.filter and args.filter_file != 'auto':
     print('Warning: Filter flag is not set so filter file will not be created')
+
+if args.output == 'auto' or (args.filter and args.filter_file == 'auto'):
+    # Set up data directory
+    cwd = os.getcwd()
+    path = os.path.join(cwd, 'data')
+    date_time = strftime("%Y.%m.%d_%H.%M.%S", gmtime())
+    path = os.path.join(path, date_time)
+    _create_dir(path)
+
+# Generate appropriate output and filter file paths
+if args.output == 'auto':
+    output_path = os.path.join(path, '{}_{}.txt'.format(args.quench,
+                                                        args.nevt))
+else:
+    output_path = args.output
+if args.filter_file == 'auto':
+    filter_path = os.path.join(path, '{}_{}_filtered.txt'.format(args.quench,
+                                                            args.nevt))
+else:
+    filter_path = args.filter_file
 
 # Check last to prevent changing files on the system when other
 # arguments might still cause a premature exit
 try:
-    f = open(args.output, 'w')
+    f = open(output_path, 'w')
 except Exception:
     raise ValueError('Error while opening output file')
 
 if args.filter:
     try:
-        g = open(args.filter_file, 'w')
+        g = open(filter_path, 'w')
     except Exception:
         raise ValueError('Error while opening file for filtered results')
 
@@ -135,9 +170,9 @@ SJpTmin = args.SJpTmin
 SJradius = args.SJradius
 output = args.output
 filter_results = args.filter
-filter_file = args.filter_file
+filter_file = filter_path
 
-# PYTHIA without QCD and QED does nothing ???
+# PYTHIA without QCD and QED does nothing
 if args.QCDoff and args.QEDoff:
     pythia_on = False
 
@@ -145,30 +180,22 @@ if args.QCDoff and args.QEDoff:
 
 # Load/generate TRENTO data for 100,000 Au Au events
 if trento_file == 'off':
-    trento_on = False
-    b = np.zeros(1e5)
-    Npart = np.zeros(1e5)
-    mult = np.zeros(1e5)
-    e2 = np.zeros(1e5)
+    proc = subprocess.Popen('trento Au Au {}'.format(nevt).split(),
+                            stdout=subprocess.PIPE)
+    trento_data = np.array([l.split() for l in proc.stdout], dtype=float)
 else:
     try:
-        data = np.loadtxt(trento_file)
+        trento_data = np.loadtxt(trento_file)
     except Exception as e:
         # At this point the file should exist
         # Being unable to open the file means it is being used
         # Or the user does not have permission to open it
         print('Unable to open TRENTO file.')
         raise e
-        sys.exit()
 
-    # Conversion from fit to PHENIX 200 GeV data
-    mult_scale = 4.66
-
-    # data = [[event_number, impact_param, Npart, mult, e2, e3, e4, e5]]
-    b = data[:, 1]
-    Npart = data[:, 2]
-    mult = mult_scale * data[:, 3]
-    e2 = data[:, 4]
+# trento_data = [[event_number, impact_param, Npart, mult, e2, e3, e4, e5]]
+# Conversion from fit to PHENIX 200 GeV data
+mult_scale = 4.66
 
 # -------------------------- PHYSICAL CONSTANTS ---------------------- #
 
@@ -177,6 +204,7 @@ T = 0.15
 
 # ???
 deta = 4
+
 # set pion mass to 0.14 GeV
 mpi = 0.14
 
@@ -218,7 +246,7 @@ pythia.readString('Random:seed = {}'.format(seed))
 # Initialize PYTHIA with given settings
 pythia.init()
 
-# Prepare output file ???
+# Prepare output file
 header = '''# Input parameters: to be listed here
 #       trento-stat          |      pythia-truth                 | slowjet
 # iev mult xgj quench b Np e2   part1 pT eta phi part2 pT eta phi   Njets pT eta phi pT eta phi
@@ -228,17 +256,13 @@ f.write(header)
 if filter_results:
     g.write(header)
 
-proc = subprocess.Popen('trento Au Au'.split(), stdout=subprocess.PIPE)
-trento_data = np.array([l.split() for l in proc.stdout][0], dtype=float)
-mult = int(trento_data[3])
-b = trento_data[1]
-Npart = int(trento_data[2])
-e2 = trento_data[4]
-
-
 # Loop over events, start with PYTHIA then add TRENTO to PYTHIA event
-i = trento_seed
-while i < trento_seed + nevt:
+i = 0
+while i < nevt:
+    mult = int(mult_scale * trento_data[i][3])
+    b = trento_data[i][1]
+    Npart = int(trento_data[i][2])
+    e2 = trento_data[i][4]
     problem = False
 
     # Reset event data ???
@@ -287,6 +311,7 @@ while i < trento_seed + nevt:
         for jet in (purejet, quenchedjet):
             if jet.id() < 1 or jet.id() > 99:
                 problem = True
+                print('Problem with jet id found: {}'.format(jet.id()))
 
         # Rescale quenchedjet daughters by momentum, not same as energy
         daughters = []
@@ -327,7 +352,7 @@ while i < trento_seed + nevt:
                              'n': (0.940, 2112),
                              'nbar': (0.940, -2112)}
 
-            # Particle selected randomly
+            # Particle selected randomly (still needs to be changed)
             if p_sample <= 0.11:
                 particle_name = 'pi+'
             elif 0.11 < p_sample <= 0.22:
@@ -444,12 +469,6 @@ while i < trento_seed + nevt:
     else:
         f.write(output)
         i += 1
-        proc = subprocess.Popen('trento Au Au'.split(), stdout=subprocess.PIPE)
-        trento_data = np.array([l.split() for l in proc.stdout][0], dtype=float)
-        mult = int(trento_data[3])
-        b = trento_data[1]
-        Npart = int(trento_data[2])
-        e2 = trento_data[4]
 
 f.close()
 if args.filter:
