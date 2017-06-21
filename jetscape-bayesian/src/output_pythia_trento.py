a6469bfa1e021f75d59e8101d6ee6e1d5bbcf182
Matthias Heinz <heinz.matthias.3@gmail.com>
2017-06-20 17:33:14 -0700

    Implement Scott's trick for TRENTO noise in PYTHIA event generation

--- jetscape-bayesian/src/output_pythia_trento.py
+++ jetscape-bayesian/src/output_pythia_trento.py
@@ -13,7 +13,7 @@ import argparse
 # -------------------------- ARGUMENT PARSING ------------------------ #
 
 prog_description = '''
-Generates output below for pythia jets in trento background with
+Generates output below for PYTHIA jets in TRENTO background with
 slowjet reconstruction.'''.replace('\n', '', 1)
 prog_epilogue = 'Writes output to output.txt, see header for format.'
 
@@ -27,13 +27,13 @@ parser = argparse.ArgumentParser(
 # Add arguments
 parser.add_argument('-f', '--file', type=str, metavar='TRENTO_FILE',
                     default='../data/AuAu_200GeV_100k.txt',
-                    help='path to trento data file or turn \"off\"')
+                    help='path to TRENTO data file or turn \"off\"')
 parser.add_argument('-e', '--eCM', type=float, default=200.0,
                     metavar='BEAM_ENERGY',
                     help='PYTHIA beam center-of-mass energy (in GeV)')
 parser.add_argument('-n', '--nevt', type=int, default=10,
                     metavar='NUMBER_OF_EVENTS',
-                    help='number of PYTHIA and trento events to generate')
+                    help='number of PYTHIA and TRENTO events to generate')
 parser.add_argument('-c', '--QCDoff', action='store_true',
                     help='turn PYTHIA hard QCD processes off')
 parser.add_argument('-q', '--QEDoff', action='store_true',
@@ -66,7 +66,7 @@ args = parser.parse_args()
 # Check validity of arguments
 if args.file != 'off':
     if not os.path.isfile(args.file):
-        raise ValueError('trento file {} not found.'.format(args.file))
+        raise ValueError('TRENTO file {} not found.'.format(args.file))
 
 if args.eCM <= 0.0:
     raise ValueError('Beam energy must be positive')
@@ -114,7 +114,7 @@ if args.filter:
     except Exception:
         raise ValueError('Error while opening file for filtered results')
 
-# Default settings for Trento data
+# Default settings for TRENTO data
 trento_on = True
 trento_seed = 0
 pythia_on = True
@@ -125,7 +125,7 @@ eCM = args.eCM
 pTHatMin = args.pTHatMin
 pTHatMax = args.pTHatMax
 seed = args.seed
-bool_on_dict = { False: 'on', True: 'off'}
+bool_on_dict = {False: 'on', True: 'off'}
 QCD = bool_on_dict[args.QCDoff]
 QED = bool_on_dict[args.QEDoff]
 quench = args.quench
@@ -142,7 +142,7 @@ if args.QCDoff and args.QEDoff:
 
 # ------------------------- DATA LOADING ----------------------------- #
 
-# Load/generate trento data for 100,000 Au Au events
+# Load/generate TRENTO data for 100,000 Au Au events
 if trento_file == 'off':
     trento_on = False
     b = np.zeros(1e5)
@@ -156,7 +156,7 @@ else:
         # At this point the file should exist
         # Being unable to open the file means it is being used
         # Or the user does not have permission to open it
-        print('Unable to open trento file.')
+        print('Unable to open TRENTO file.')
         raise e
         sys.exit()
 
@@ -173,6 +173,7 @@ else:
 
 # temperature in GeV
 T = 0.15
+
 # ???
 deta = 4
 # set pion mass to 0.14 GeV
@@ -188,7 +189,7 @@ rho_2 = 0.15
 
 # --------------------------- PYTHIA RUNS ---------------------------- #
 
-# Initialize Pythia
+# Initialize PYTHIA
 pythia = pythia8.Pythia()
 
 # Set PYTHIA output mode to quiet
@@ -226,7 +227,7 @@ f.write(header)
 if filter_results:
     g.write(header)
 
-# Loop over events, start with pythia then add trento to pythia event
+# Loop over events, start with PYTHIA then add TRENTO to PYTHIA event
 i = trento_seed
 while i < trento_seed + nevt:
     problem = False
@@ -236,6 +237,9 @@ while i < trento_seed + nevt:
     if pythia_on:
         # Generate new PYTHIA event ???
         pythia.next()
+
+        # for j in range(pythia.event.size()):
+        #     print(pythia.event[j].list(False, False, 3))
 
         # Initial hard partons are stored in pythia.event [5] and [6] UNUSED
         # daughters5 = []
@@ -271,8 +275,8 @@ while i < trento_seed + nevt:
 
         # TODO introduce checks on hard-scattered particle assignments
         # First check 0<jet.id()<100
-        for jet in (purejet,quenchedjet):
-            if (jet.id()<1 or jet.id()>99):
+        for jet in (purejet, quenchedjet):
+            if jet.id() < 1 or jet.id() > 99:
                 problem = True
 
         # Rescale quenchedjet daughters by momentum, not same as energy
@@ -294,26 +298,15 @@ while i < trento_seed + nevt:
             prt.pz(pz)
             prt.e(prt_e)
 
-    # The Trento particle production below is un-physical,
+    # The TRENTO particle production below is un-physical,
     # put together by last year's summer student.
     # It will need to be re-written at some point.
     if trento_on:
         for j in range(int(mult[i])):
-            r1, r2, r3, r4, r5 = np.random.random(5)
-            while r5 > 0.99:
-                r5 = np.random.random(1)
 
-            # pT = transverse momentum
-            pT_r1 = T*(math.sqrt(-2*math.log(r1)))
-
-            # phi = azimuthal angle
-            phi_r2 = 2*(math.pi)*(r2 - 0.5)
-
-            # eta = pseudo-rapidity
-            eta_r3 = deta*(r3 - 0.5)
-
-            # rho = normalized radial distance
-            rho_r4 = r4**0.5
+            p_sample = 1
+            while p_sample > 0.99:
+                p_sample = np.random.random()
 
             particle_dict = {'pi+': (0.140, 211),
                              'pi-': (0.140, -211),
@@ -325,49 +318,63 @@ while i < trento_seed + nevt:
                              'n': (0.940, 2112),
                              'nbar': (0.940, -2112)}
 
-            # particle selected randomly
-            if r5 <= 0.11:
+            # Particle selected randomly
+            if p_sample <= 0.11:
                 particle_name = 'pi+'
-            elif 0.11 < r5 <= 0.22:
+            elif 0.11 < p_sample <= 0.22:
                 particle_name = 'pi-'
-            elif 0.22 < r5 <= 0.33:
+            elif 0.22 < p_sample <= 0.33:
                 particle_name = 'pi0'
-            elif 0.33 < r5 <= 0.44:
+            elif 0.33 < p_sample <= 0.44:
                 particle_name = 'K+'
-            elif 0.44 < r5 <= 0.55:
+            elif 0.44 < p_sample <= 0.55:
                 particle_name = 'K-'
-            elif 0.55 < r5 <= 0.66:
+            elif 0.55 < p_sample <= 0.66:
                 particle_name = 'p'
-            elif 0.66 < r5 <= 0.77:
+            elif 0.66 < p_sample <= 0.77:
                 particle_name = 'pbar'
-            elif 0.77 < r5 <= 0.88:
+            elif 0.77 < p_sample <= 0.88:
                 particle_name = 'n'
-            elif 0.88 < r5 <= 0.99:
+            elif 0.88 < p_sample <= 0.99:
                 particle_name = 'nbar'
             else:
-                raise Exception('Invalid particle value: {}'.format(r5))
+                raise Exception('Invalid particle value: {}'.format(p_sample))
             mass, pid = particle_dict[particle_name]
 
-            # calculate initial transverse rapidity (yT)
-            eT = (mass*mass+pT_r1*pT_r1)**0.5
-            yT = 0.5 * np.log((eT+pT_r1)/(eT-pT_r1))
-            pT_initial = pT_r1
-            yT_initial = yT
+            r1, r2, r3 = np.random.random(3)
 
-            # apply flow as additive boost to transverse rapidity
-            yBoost = rho_r4*rho_0 + rho_2*e2[i]*np.cos(2*phi_r2)
-            yT = yT_initial + yBoost
+            # Compute 3-momentum magnitude
+            pmag = -1 * T * math.log(r1 * r2 * r3)
 
-            # convert back to pT
-            pT_wflow = mass*np.cosh(yT)
+            # Compute relativistic energy
+            E = math.sqrt(pmag * pmag + mass * mass)
 
-            # add particles to the pythia event list
-            px = pT_wflow * math.cos(phi_r2)
-            py = pT_wflow * math.sin(phi_r2)
-            pz = pT_wflow * math.sinh(eta_r3)
-            E = (pT_wflow**2 + pz**2 + mass**2)**0.5
-            pythia.event.append(pid, 200, 0, 0, px, py, pz, E, mass, 0.0,
-                                9.0)
+            # Check results against boltzmann distribution
+            b_sample = np.random.random()
+            while b_sample >= math.exp((pmag - E) / T):
+                # Get new values until check is passed
+                r1, r2, r3 = np.random.random(3)
+                pmag = -1 * T * math.log(r1 * r2 * r3)
+                E = math.sqrt(pmag * pmag + mass * mass)
+                b_sample = np.random.random()
+
+            # Compute theta and phi based on "Scott's Tick for Boltzmann 3D"
+            ctheta = math.log(r1 / r2) / math.log(r1 * r2)
+            stheta = math.sqrt(1 - ctheta * ctheta)
+            phi = T * T * (math.log(r1 * r2)**2) / (pmag * pmag)
+            if phi > 1.0:
+                raise ValueError('phi={}, out of range'.format(phi))
+            phi = 2.0 * math.pi * phi
+
+            # Compute 3-mom components
+            pz = pmag * ctheta
+            px = pmag * stheta * math.cos(phi)
+            py = pmag * stheta * math.sin(phi)
+
+            # Append TRENTO event to PYTHIA event record
+            # particle id, status, mothers, daughters, px, py, pz, E, m,
+            # scaleIn, polIn
+            pythia.event.append(pid, 200, 0, 0, px, py, pz, E, mass, 0.0, 9.0)
 
     # Initialize and call SlowJet
     etaMax = 4.
@@ -381,7 +388,7 @@ while i < trento_seed + nevt:
     else:
         xgj = slowJet.pT(1)/slowJet.pT(0)
 
-    # Prepare output, stats include trento information
+    # Prepare output, stats include TRENTO information
     # (values set to zero if not turned on)
     stats_output = '{: 5d}'.format(i) \
                  + '{: 6d}'.format(int(mult[i]) + pythia_mult) \
@@ -428,7 +435,7 @@ while i < trento_seed + nevt:
     else:
         f.write(output)
         i += 1
-        
+
 f.close()
 if args.filter:
     g.close()
