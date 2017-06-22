c2e0f10c1f116a39b1366e464b10eec3752c61f1
Matthias Heinz <heinz.matthias.3@gmail.com>
2017-06-21 17:57:29 -0700

    Adjust style to match pycodestyle

--- systematic-error-exploration/src/load_covariance_matrix.py
+++ systematic-error-exploration/src/load_covariance_matrix.py
@@ -1,28 +1,35 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-# from __future__ import unicode_literals
 from fudge.gnd.covariances import base
-from xData import array, gridded
-from xData import axes as axesModule, values as valuesModule, link as linkModule
+from xData import array
+from xData import gridded
+from xData import axes as axesModule
+from xData import values as valuesModule
+from xData import link as linkModule
 from hepdata_manager import HEPData_Manager
 import numpy as np
 
 
-def get_GND_covariance(arr, energyBounds):
-    GNDarray = array.full(shape=arr.shape, data=arr[ np.tril_indices(len(arr)) ], symmetry='lower')
+def get_GND_covariance(arr, energyBounds, name='default'):
+    GNDarray = array.full(shape=arr.shape, data=arr[np.tril_indices(len(arr))],
+                          symmetry='lower')
 
-    axes = axesModule.axes(labelsUnits = { 0 : ('matrix_elements', ''), 
-                                                1 : ('column_energy_bounds', 'GeV/c'),
-                                                2 : ('row_energy_bounds', 'GeV/c') })
+    axes = axesModule.axes(labelsUnits={ 0 : ('matrix_elements', ''), 
+                                         1 : ('column_energy_bounds', 'GeV/c'),
+                                         2 : ('row_energy_bounds', 'GeV/c') })
     axes[2] = axesModule.grid(axes[2].label, axes[2].index, axes[2].unit, 
-                style = axesModule.boundariesGridToken, values = valuesModule.values(energyBounds))
+                              style=axesModule.boundariesGridToken,
+                              values=valuesModule.values(energyBounds))
     axes[1] = axesModule.grid(axes[1].label, axes[1].index, axes[1].unit, 
-            style = axesModule.linkGridToken, values = linkModule.link(link = axes[2].values, relative = True))
+                              style=axesModule.linkGridToken,
+                              values=linkModule.link(link=axes[2].values,
+                                                     relative=True))
 
     GNDgridded = gridded.gridded2d(axes, GNDarray)
 
-    covariance = base.covarianceMatrix("example", type="absolute", matrix=GNDgridded)
+    covariance = base.covarianceMatrix(name, type="absolute",
+                                       matrix=GNDgridded)
     return covariance
 
 alice_event = 'ins1394678'
@@ -51,7 +58,7 @@ stat_cov_mat = np.diag([x**2 for x in stat_err])
 
 corr_cov_mat = np.array([[0.75 * x * y for x in corr_err] for y in corr_err])
 
-stat_cov = get_GND_covariance(stat_cov_mat, indep)
-corr_cov = get_GND_covariance(corr_cov_mat, indep)
+stat_cov = get_GND_covariance(stat_cov_mat, indep, 'statistical')
+corr_cov = get_GND_covariance(corr_cov_mat, indep, 'correlated')
 print('\n'.join(stat_cov.toXMLList()))
 print('\n'.join(corr_cov.toXMLList()))
