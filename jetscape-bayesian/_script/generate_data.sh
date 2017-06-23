fdfae8f156955397c529ba84194bbe5eb5d29ccc
Matthias Heinz <heinz.matthias.3@gmail.com>
2017-06-22 17:38:12 -0700

    Update generate_data script to reflect changes

--- jetscape-bayesian/_script/generate_data.sh
+++ jetscape-bayesian/_script/generate_data.sh
@@ -14,8 +14,8 @@ echo "#       trento-stat          |      pythia-truth                 | slowjet
 echo "# iev mult xgj quench b Np e2   part1 pT eta phi part2 pT eta phi   Njets pT eta phi pT eta phi" >> data/full_data.txt
 
 QUENCH_FACTOR=.05
-while [[ $(echo $QUENCH_FACTOR'<1.0' | bc -l) = 1 ]]; do
-    python3 src/output_pythia_trento.py -f data/AuAu_200GeV_100k.txt -o data/"0$QUENCH_FACTOR".txt -u "0$QUENCH_FACTOR" -n "$SIZE" -c -w -x data/filtered_"0$QUENCH_FACTOR".txt
+while [[ $(echo $QUENCH_FACTOR'<1.05' | bc -l) = 1 ]]; do
+    python3 src/output_pythia_trento.py -f off -o data/"0$QUENCH_FACTOR".txt -u "0$QUENCH_FACTOR" -n "$SIZE" -c -w -x data/filtered_"0$QUENCH_FACTOR".txt
     tail -n "$SIZE" data/"0$QUENCH_FACTOR".txt >> data/full_data.txt
     echo "0$QUENCH_FACTOR complete"
     QUENCH_FACTOR=$(echo 0$QUENCH_FACTOR'+0.05'| bc)
