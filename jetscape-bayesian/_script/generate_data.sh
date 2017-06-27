27e96ba7dbfb907c0a7c62c5f8eeb0ee41decbe8
Matthias Heinz <heinz.matthias.3@gmail.com>
2017-06-26 17:47:57 -0700

    Make options in script explicit (long)

--- jetscape-bayesian/_script/generate_data.sh
+++ jetscape-bayesian/_script/generate_data.sh
@@ -13,9 +13,9 @@ echo "# Input parameters: to be listed here" > data/full_data.txt
 echo "#       trento-stat          |      pythia-truth                 | slowjet" >> data/full_data.txt
 echo "# iev mult xgj quench b Np e2   part1 pT eta phi part2 pT eta phi   Njets pT eta phi pT eta phi" >> data/full_data.txt
 
-QUENCH_FACTOR=.05
+QUENCH_FACTOR=.15
 while [[ $(echo $QUENCH_FACTOR'<1.05' | bc -l) = 1 ]]; do
-    python3 src/output_pythia_trento.py -f off -o data/"0$QUENCH_FACTOR".txt -u "0$QUENCH_FACTOR" -n "$SIZE" -c -w -x data/filtered_"0$QUENCH_FACTOR".txt
+    python3 src/output_pythia_trento.py -f off -o data/"0$QUENCH_FACTOR".txt --quench "0$QUENCH_FACTOR" --nevt "$SIZE" --QCDoff --SJpTmin 5.0
     tail -n "$SIZE" data/"0$QUENCH_FACTOR".txt >> data/full_data.txt
     echo "0$QUENCH_FACTOR complete"
     QUENCH_FACTOR=$(echo 0$QUENCH_FACTOR'+0.05'| bc)
