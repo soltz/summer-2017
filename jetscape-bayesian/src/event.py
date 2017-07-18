0d951c8335c65427f267602f7ee1aaf2a84598e4
Matthias Heinz <heinz.matthias.3@gmail.com>
2017-07-18 14:36:38 -0700

    Move background into event.py to collect similar utilities together

--- jetscape-bayesian/src/event.py
+++ jetscape-bayesian/src/event.py
@@ -7,11 +7,21 @@ import errno
 import numpy as np
 from time import gmtime, strftime
 import pythia8
+import subprocess
 
 
 class EventGenerator(object):
 
-    def __init__(self, save_directory, pythia_settings):
+    def __init__(self, save_directory, pythia_settings=None):
+        pass
+
+    def generate(self):
+        return ''
+
+
+class BackgroundGenerator(object):
+
+    def __init__(self, projectile1, projectile2):
         pass
 
     def generate(self):
