067faab901e152e2c956514e4426c342faae78f3
Matthias Heinz <heinz.matthias.3@gmail.com>
2017-07-18 15:05:48 -0700

    Add initial pydoc documentation to event.py

--- jetscape-bayesian/src/event.py
+++ jetscape-bayesian/src/event.py
@@ -1,3 +1,38 @@
+# Author: Matthias Heinz
+"""
+High-energy nucleus-nucleus event data simulation module.
+
+Abstracted interface to PYTHIA for jet simulation and TRENTO for background
+simulation. Full event records are saved to timestamped files, and methods
+return the absolute path to the file for later use.
+
+class EventGenerator
+--------------------
+
+Abstracted interface to PYTHIA to quickly simulate jet event records for many
+events with the same settings. The default settings are chosen to produce jets
+at RHIC energies. It has the following methods::
+
+    jet_generator = event.EventGenerator(save_directory, preset,
+                                         pythia_settings)
+    filename = jet_generator.generate()
+
+class BackgroundGenerator
+-------------------------
+
+Abstracted interface to TRENTO to quickly simulate background event records for
+many events. The default settings are chosen to match multiplicities at RHIC
+energies. It has the following methods::
+
+    background_generator = event.BackgroundGenerator(save_directory,
+                                                     projectile1, projectile2)
+    filename = background_generator.generate()
+
+Changelog:
+
+2017.07.18
+    Initial creation
+"""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
