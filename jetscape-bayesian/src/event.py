b77d03909a13d9508eaf5635837d5c3f90e2e553
Matthias Heinz <heinz.matthias.3@gmail.com>
2017-07-18 11:58:33 -0700

    Add stubs for class methods for event.py

--- jetscape-bayesian/src/event.py
+++ jetscape-bayesian/src/event.py
@@ -7,3 +7,12 @@ import errno
 import numpy as np
 from time import gmtime, strftime
 import pythia8
+
+
+class EventGenerator(object):
+
+    def __init__(self, save_directory, pythia_settings):
+        pass
+
+    def generate(self):
+        return ''
